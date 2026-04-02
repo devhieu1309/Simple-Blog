b93dbc0756935ad2361b1e4b44e7b1fbe5496a49
pmhieu1309 <pmhieudev1309@gmail.com>
2026-04-02 22:04:58 +0700

    Model relation User/Post/Category/Tag.

---
+++ app/Models/Post.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
+use App\Models\Category;
+use App\Models\Tag;
+
+class Post extends Model
+{
+    protected $fillable = [
+        'title',
+        'content',
+        'image',
+        'category_id',
+        'user_id',
+    ];
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+    public function category() {
+        return $this->belongsTo(Category::class);
+    }
+
+   public function tags() {
+     return $this->belongsToMany(Tag::class);
+   }
+}
